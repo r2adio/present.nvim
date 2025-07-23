558094b45915ef59495a479e5d19a346190fe8f0
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-07-23 22:35:11 +0530

    read md from new buffer, use floating_window - can read from a nvim buffer, by using its buffer number - uses floating window to display different slides

--- lua/present.lua
+++ lua/present.lua
@@ -1,11 +1,40 @@
 local M = {}
 
+local function create_floating_window(opts)
+	opts = opts or {}
+	local width = opts.width or math.floor(vim.o.columns * 0.8)
+	local height = opts.width or math.floor(vim.o.lines * 0.8)
+
+	-- calculate the position to center the window
+	local col = math.floor((vim.o.columns - width) / 2)
+	local row = math.floor((vim.o.lines - width) / 2)
+
+	-- create a buffer
+	local buf = vim.api.nvim_create_buf(false, true) -- no file, scratch buffer
+
+	-- define window configuration
+	local win_config = {
+		relative = "editor",
+		width = width,
+		height = height,
+		col = col,
+		row = row,
+		style = "minimal",
+		border = "rounded",
+	}
+
+	-- create the floatin window
+	local win = vim.api.nvim_open_win(buf, true, win_config)
+
+	return { buf = buf, win = win }
+end
+
 M.setup = function()
 	-- ...
 end
 
 ---@class present.Slides
----@field slides string[]
+---@field slides string[]: The slides of the file
 
 -- Takes some lines and parses them
 ---@param lines string[]: The lines in the buffer
@@ -16,7 +45,7 @@ local parse_slides = function(lines)
 	local separator = "^#"
 
 	for _, line in ipairs(lines) do
-		print(line, "find:", line:find(separator), "|")
+		-- print(line, "find:", line:find(separator), "|")
 		if line:find(separator) then
 			if #current_slide > 0 then
 				table.insert(slides.slides, current_slide)
@@ -29,11 +58,37 @@ local parse_slides = function(lines)
 	return slides
 end
 
-vim.print(parse_slides({
-	"plugin name: present.nvim",
-	"# here is H1",
-	"body of H1",
-	"## here is the H2",
-	"body of H2",
-}))
+M.start_presentation = function(opts)
+	opts = opts or {}
+	opts.bufnr = opts.bufnr or 0
+
+	local lines = vim.api.nvim_buf_get_lines(opts.bufnr, 0, -1, false)
+	local parsed = parse_slides(lines)
+	local float = create_floating_window()
+
+	local current_slide = 1
+	vim.keymap.set("n", "n", function()
+		current_slide = math.min(current_slide + 1, #parsed.slides)
+		vim.api.nvim_buf_set_lines(float.buf, 0, -1, false, parsed.slides[current_slide])
+	end, { buffer = float.buf })
+	vim.keymap.set("n", "p", function()
+		current_slide = math.max(current_slide - 1, 1)
+		vim.api.nvim_buf_set_lines(float.buf, 0, -1, false, parsed.slides[current_slide])
+	end, { buffer = float.buf })
+	vim.keymap.set("n", "q", function()
+		vim.api.nvim_win_close(float.win, true)
+	end, { buffer = float.buf })
+
+	vim.api.nvim_buf_set_lines(float.buf, 0, -1, false, parsed.slides[1])
+end
+
+M.start_presentation({ bufnr = 3 }) -- :echo nvim_get_current_buf
+-- vim.print(parse_slides({
+-- 	"plugin name: present.nvim",
+-- 	"# here is H1",
+-- 	"body of H1",
+-- 	"## here is the H2",
+-- 	"body of H2",
+-- }))
+--
 return M
