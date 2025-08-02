15f19aeda92c4c983985f817a5338371a7b1036a
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-08-03 04:56:00 +0530

    feat: add custom command for startup

--- plugin/load_present.lua
+++ plugin/load_present.lua
@@ -1,3 +1,5 @@
 -- to autoamtically to the plugin at startup
-require("present")
+vim.api.nvim_create_user_command("Present", function()
+	require("present").start_presentation()
+end, {})
 -- or, above line can be added in user config
